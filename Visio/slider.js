7e39c527a0f0748eb39c512e583b2b5fca50441a
unknown <zenandenocanda16@gmail.com>
2025-04-24 16:26:00 +0200

    day1

---
+++ Visio/slider.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    let currentSlide = 0;
+    const slides = $(".slide");
+    const dots = $(".dot");
+    const totalSlides = slides.length;
+  
+    function showSlide(index) {
+      slides.removeClass("active").eq(index).addClass("active");
+      dots.removeClass("active").eq(index).addClass("active");
+    }
+  
+    $(".arrow.right").click(function () {
+      currentSlide = (currentSlide + 1) % totalSlides;
+      showSlide(currentSlide);
+    });
+  
+    $(".arrow.left").click(function () {
+      currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+      showSlide(currentSlide);
+    });
+  
+    $(".dot").click(function () {
+      currentSlide = $(this).index();
+      showSlide(currentSlide);
+    });
+  });
+  
